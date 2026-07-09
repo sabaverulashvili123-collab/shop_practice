4ba05477bdc4b7ac0b9fdeab4bdc9e0599c9f65f
Saba <sabaverulashvili123@gmail.com>
2026-07-09 23:05:08 +0400

    Add More SQL simple queries.

--- 03_queries/04_practice_queries.sql
+++ 03_queries/04_practice_queries.sql
@@ -43,3 +43,27 @@ SELECT name, email
 FROM customers 
 WHERE email LIKE '%@%';
 
+SELECT customer_id, COUNT(*) AS orders_count
+FROM orders
+GROUP BY customer_id;
+
+SELECT status, COUNT(*) AS status_count
+FROM orders
+GROUP BY status;
+
+SELECT order_id, SUM(quantity) AS total_quantity 
+FROM order_items
+GROUP BY order_id;
+ 
+SELECT product_id, SUM(quantity) AS total_sold
+FROM order_items
+GROUP BY product_id;
+
+SELECT product_id, SUM(quantity) AS total_quantity,
+COUNT(order_id) AS orders_count
+FROM order_items
+GROUP BY product_id;
+
+SELECT category, AVG(price) AS average_price, COUNT(*) AS product_count
+FROM products 
+GROUP BY category;
