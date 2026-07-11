bb419050b462c446bbc4f80e6e6cc8a5bb2c8170
Saba <sabaverulashvili123@gmail.com>
2026-07-11 14:09:25 +0400

    basic queries before subqueries.

--- 03_queries/04_practice_queries.sql
+++ 03_queries/04_practice_queries.sql
@@ -114,3 +114,99 @@ COUNT(*) AS times_ordered
 FROM order_items
 GROUP BY product_id;
 
+SELECT product_id, COUNT(*) AS times_ordered
+FROM order_items
+WHERE quantity >= 2
+GROUP BY product_id
+HAVING COUNT(*) >= 2;
+
+SELECT id,product_name, price 
+FROM products;
+
+SELECT * 
+FROM orders
+WHERE status = 'Completed'
+ORDER BY order_date DESC;
+
+ SELECT customer_id, COUNT(order_id) AS orders_count
+FROM orders
+GROUP BY customer_id;
+
+
+ SELECT * FROM products
+ WHERE price BETWEEN 30 AND 150
+ ORDER BY price ASC;
+
+SELECT id, name, email
+FROM customers
+WHERE name LIKE 'A%';
+
+SELECT product_id, COUNT(product_id) AS product_amount
+FROM order_items
+GROUP BY product_id;
+
+SELECT product_id,  SUM(quantity) AS total_quantity
+FROM order_items
+GROUP BY product_id;
+
+SELECT product_id, MIN(price) AS cheapest_price,
+MAX(price) AS highest_price, 
+AVG(price) AS avg_price
+FROM order_items
+GROUP BY product_id;
+
+SELECT customer_id, COUNT(*) AS total_orders
+FROM orders
+GROUP BY customer_id
+ORDER BY COUNT(*) DESC;
+
+SELECT product_id, COUNT(product_id) AS  total_amount,
+SUM(quantity) AS total_sold,
+SUM(quantity * price) AS total_revenue,
+AVG(price) AS avg_price
+FROM order_items
+GROUP BY product_id
+ORDER BY SUM(quantity * price) DESC;
+
+SELECT * FROM order_items
+WHERE price > 100;
+
+SELECT product_id, COUNT(*) AS total_count
+FROM order_items
+GROUP BY product_id
+HAVING COUNT(*) > 2;
+
+SELECT product_id, COUNT(*) AS total_count
+FROM order_items
+WHERE price > 100
+GROUP BY product_id
+HAVING COUNT(*) > 2 ;
+
+SELECT product_id, COUNT(*) AS total_count,
+SUM(quantity) AS total_amount, AVG(price) AS avg_price,
+MAX(price) AS highest_price
+FROM order_items
+WHERE price >= 50
+GROUP BY product_id
+HAVING SUM(quantity) > 15
+ORDER BY total_amount DESC;
+
+SELECT product_id, COUNT(*) AS total_count,
+ SUM(quantity * price) AS total_revenue
+FROM order_items
+WHERE quantity >= 2
+GROUP BY product_id
+HAVING SUM(quantity * price) > 1000
+ORDER BY total_revenue DESC, product_id ASC;
+
+SELECT product_id, COUNT(*) AS total_orders,
+SUM(quantity) AS total_quantity,
+SUM(quantity * price) AS total_revenue,
+AVG(price) AS avg_price,
+MIN(price) AS cheapest_price,
+MAX(price) AS highest_price
+FROM order_items
+WHERE price BETWEEN 50 AND 300
+GROUP BY product_id
+HAVING COUNT(*) >= 3  AND SUM(quantity) > 10
+ORDER BY total_revenue DESC, avg_price DESC;
